e6db5901a9254eb701389862d5dd2b64bee1a4a0
thanhlv <lethanh9398@gmail.com>
2025-05-11 00:10:06 +0700

    Bump version to 1.0.2 and adjust delay for messenger box detection

--- gaubong-us/script_auto_send_first_tin_nhan.js
+++ gaubong-us/script_auto_send_first_tin_nhan.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Script auto send fist tin nhắn gấu bông
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://gaubong.us/users/online/nu*
